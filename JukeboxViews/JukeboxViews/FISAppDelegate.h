3c6d65987771ae31a027c85b111146d99ea0a74d
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-10-09 14:04:59 -0400

    cleanup- add base project

---
+++ JukeboxViews/JukeboxViews/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  JukeboxViews
+//
+//  Created by Chris Gonzales on 8/21/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JukeboxViews/JukeboxViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JukeboxViews
+//
+//  Created by Chris Gonzales on 8/21/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FISAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
